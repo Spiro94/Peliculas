491e428b9d66cd1786f73ca52fe02e22731e0247
Daniel Villamizar <danieljosevillamizar@gmail.com>
2019-10-01 19:01:08 -0500

    Initial commit

---
+++ lib/src/providers/peliculas_provider.dart
@@ -0,0 +1,102 @@
+import 'dart:async';
+
+import 'package:peliculas/src/models/actores_model.dart';
+import 'package:peliculas/src/models/pelicula_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class PeliculasProvider {
+  String _apikey = 'ff327d5428253b3dbdab14ec10037979';
+  String _url = 'api.themoviedb.org';
+  String _language = 'es-ES';
+  int _popularesPage = 0;
+
+  bool _cargando = false;
+
+  List<Pelicula> _populares = new List();
+
+//Sin el broadcast solo puede tener un Listener
+  final _popularesStreamController =
+      StreamController<List<Pelicula>>.broadcast();
+
+  Function(List<Pelicula>) get popularesSink =>
+      _popularesStreamController.sink.add;
+
+  Stream<List<Pelicula>> get popularesStream =>
+      _popularesStreamController.stream;
+
+  void disposeStreams() {
+    _popularesStreamController?.close();
+  }
+
+  Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
+    final resp = await http.get(url);
+    //print(url);
+    final decodedData = json.decode(resp.body);
+
+    // print(decodedData['results']);
+
+    final peliculas = new Peliculas.fromJsonList(decodedData['results']);
+
+    // print(peliculas.items[0].title);
+
+    return peliculas.items;
+  }
+
+  Future<List<Pelicula>> getEnCines() async {
+    final url = Uri.https(_url, '3/movie/now_playing',
+        {'api_key': _apikey, 'language': _language});
+
+    return await _procesarRespuesta(url);
+  }
+
+  Future<List<Pelicula>> getPopulares() async {
+    if (_cargando) return [];
+
+    _cargando = true;
+
+    _popularesPage++;
+
+    // print('Cargando los siguientes');
+
+    final url = Uri.https(_url, '3/movie/popular', {
+      'api_key': _apikey,
+      'language': _language,
+      'page': _popularesPage.toString(),
+    });
+
+    final resp = await _procesarRespuesta(url);
+
+    _populares.addAll(resp);
+    popularesSink(_populares);
+
+    _cargando = false;
+
+    return resp;
+  }
+
+  Future<List<Actor>> getCast(String peliId) async {
+    final url = Uri.https(_url, '3/movie/$peliId/credits', {
+      'api_key': _apikey,
+      'language': _language,
+    });
+
+    final resp = await http.get(url);
+
+    final docodedData = json.decode(resp.body);
+
+    final cast = new Cast.fromJsonList(docodedData['cast']);
+
+    return cast.actores;
+  }
+
+  Future<List<Pelicula>> buscarPelicula(String query) async {
+    final url = Uri.https(_url, '3/search/movie', {
+      'api_key': _apikey,
+      'language': _language,
+      'query': query,
+    });
+
+    return await _procesarRespuesta(url);
+  }
+}
